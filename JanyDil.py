47851a038b3f14b2a06cc8bfa76c312e878fe650
djbrand175 <86845400+djbrand175@users.noreply.github.com>
2021-07-02 22:50:22 +0500

    Create JanyDil.py
    pkg install python
    
    pkg install python2
    
    pkg install git
    
    pip2 install requests
    
    pip2 install tqdm
    
    pip2 install mechanize
    
    git clone 
    
    githttps://github.com/djbrand175/JanyDilJanyDil.git
    
    ls
    
    cd JanyDilJanyDil
    
     python2 JANYDil.py

---
+++ JanyDil.py
@@ -0,0 +1,7 @@
+echo "# JanyDilJanyDil" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/djbrand175/JanyDilJanyDil.git
+git push -u origin main
